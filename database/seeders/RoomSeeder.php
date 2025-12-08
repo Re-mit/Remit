f2d3f005790974448c1fab32b6f2b05a13e924bf
Ohjackson <wogus3673@naver.com>
2025-12-08 14:15:08 +0900

    feat(room): add RoomSeeder for initial data

---
+++ database/seeders/RoomSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Room;
+use Illuminate\Database\Seeder;
+
+class RoomSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Room::create([
+            'name' => '622호',
+            'description' => '학과 공용 스터디룸',
+        ]);
+    }
+}
