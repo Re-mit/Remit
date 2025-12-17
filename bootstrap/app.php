353d68a8dfc447f3ce72d6c94754c357bf4bc061
jihun <yjhn0410@gmail.com>
2025-12-17 22:39:06 +0900

    feat: add google auth port

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // 내부망만 접근 허용 (Google OAuth 경로는 예외 처리: 방법 A)
+        $middleware->appendToGroup('web', \App\Http\Middleware\CheckInternalIp::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
